de096dfe1778f51c2def4ca7135711f50ea3724f
Roberto Gomez <ramdaak@yahoo.es>
2015-08-03 17:41:12 +0200

    Crear Preguntas y Validacion

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var quizController = require ('../controllers/quiz_controller.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors:[] });
 });
 
 /* GET home page. */
@@ -19,5 +19,7 @@ router.param('quizId', quizController.load); // autoload :quizId
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',                  quizController.new);
+router.post('/quizes/create',               quizController.create);
 
 module.exports = router;
